d6e1310136e8df967d4eea4826a1639033545a9b
Sinsa9706 <ox63023@gmail.com>
2024-02-20 00:46:23 +0900

    feat input system 추가

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInputController : MonoBehaviour
+public class PlayerInputController : TotalCharacterController
 {
+    [Header("부스트 속도")][Range(1f, 10f)]
+    public float speed = 1f;
 
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("OnMove" + value.ToString());
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnRun(InputValue value)
+    {
+        Debug.Log("OnRun" + value.ToString());
+        // OnMove 
+    }
 }
