42caf86ac57b0006e9a70d07070c6f23ceacc752
Sinsa9706 <ox63023@gmail.com>
2024-02-21 00:22:28 +0900

    fix 이동 코드 작성

---
+++ Assets/Scripts/Entities/Movement.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private TotalCharacterController _controller;
+
+    private Vector2 _movementDirection = Vector2.zero;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TotalCharacterController>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _movementDirection = direction;
+    }
+}
